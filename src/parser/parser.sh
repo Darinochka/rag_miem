2c81ec00a103f2eb4b3b08f4e91e8244b7f8aa8c
Darina Rustamova <ddrustamova@edu.hse.ru>
2024-04-10 21:12:16 +0300

    fix: delete /Projects links

--- src/parser/parser.sh
+++ src/parser/parser.sh
@@ -1,4 +1,4 @@
-trafilatura --crawl 1000 --recall -u https://wiki.miem.hse.ru/ | grep https://wiki.miem.hse.ru/ > data/parser/wiki_links.txt
+trafilatura --crawl 1000 --recall -u https://wiki.miem.hse.ru/ | grep https://wiki.miem.hse.ru/ | grep -iv https://wiki.miem.hse.ru/Projects/ > data/parser/wiki_links.txt
 
 trafilatura -i data/parser/wiki_links.txt  --parallel 10 --precision --json > data/wiki_miem.json
 trafilatura -i data/parser/hse_links.txt  --parallel 10 --precision --json > data/miem.json
