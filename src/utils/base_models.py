eb669f1ade05999534bb77659e6801c32082a5ac
Darina Rustamova <darisha6602@gmail.com>
2024-05-09 21:20:45 +0300

    feat: added bm25 retriever & updated telegram args

--- src/utils/base_models.py
+++ src/utils/base_models.py
@@ -6,6 +6,7 @@ from typing import Optional
 class TelegramArgs(BaseSettings):
     token: str
     retriever_host: str
+    retriever_person_host: str
     generator_host: str
     llm_name: str
 
@@ -14,3 +15,4 @@ class RetrieverArgs(BaseModel):
     embedding_model: str
     rerank_model: Optional[str]
     normalize_embeddings: bool
+    ensemble: bool = False
