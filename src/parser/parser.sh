2ba7fc9174581b08cea13df84bf04bc4626c7f50
Darina Rustamova <darisha6602@gmail.com>
2024-05-09 21:16:46 +0300

    feat: added preprocessing and parse programs

--- src/parser/parser.sh
+++ src/parser/parser.sh
@@ -1,12 +1,13 @@
-trafilatura --crawl 1000 --recall -u https://wiki.miem.hse.ru/ | grep https://wiki.miem.hse.ru/ | grep -iv https://wiki.miem.hse.ru/Projects/ > data/parser/wiki_links.txt
+trafilatura --crawl 1000 --recall -u https://wiki.miem.hse.ru/ | \
+grep https://wiki.miem.hse.ru/ | grep -iv https://wiki.miem.hse.ru/Projects/ > data/parser/wiki_links.txt
 trafilatura --links -u https://miem.hse.ru/persons  > data/parser/persons_links.txt
 
-trafilatura -i data/parser/wiki_links.txt  --parallel 10 --precision --json > data/wiki_miem.json
+trafilatura -i data/parser/wiki_links.txt  --parallel 10 --recall --json > data/wiki_miem.json
 trafilatura -i data/parser/hse_links.txt  --parallel 10 --precision --json > data/miem.json
-trafilatura -u http://pochemuchnik.hse.ru/ --precision --deduplicate --json > data/precision_pochemuchnik.json
+trafilatura -u http://pochemuchnik.hse.ru/ --recall --json > data/precision_pochemuchnik.json
 
 python3 src/parser/parse_persons.py --input_file data/parser/persons_links.txt --output_file data/persons.csv
-python3 src/parser/preprocess_persons.py --input-filename data/persons.csv --output-filename data/persons_v2.csv
+python3 src/parser/preprocess_persons.py --input-filename data/persons.csv --output-filename data/test/persons_v2.csv
 
 python3 src/parser/json2csv.py --input-filename data/wiki_miem.json --output-filename data/wiki_miem.csv
 python3 src/parser/json2csv.py --input-filename data/miem.json --output-filename data/miem.csv
